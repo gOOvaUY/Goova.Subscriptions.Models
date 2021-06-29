fd0085cad897a5dfc4aed201c8056e9e56fc2683
nicolas.cobelli <nicolas.cobelli@fing.edu.uy>
2021-06-29 14:40:43 -0300

    add commerce

--- Commerce/AddCommerceRequest.cs
+++ Commerce/AddCommerceRequest.cs
@@ -6,5 +6,7 @@
         public string CommerceNumber { get; set; }
         public string CommerceTerminalNumber { get; set; }
         public int IssuerId { get; set; }
+        public string PFCommerceId { get; set; }
+        public string PFId { get; set; }
     }
 }
